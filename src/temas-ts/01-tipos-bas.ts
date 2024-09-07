240b8bfcf479c0c56df10c804740ae82f7e340a3
Juan Andres Torres Gonzalez <jatg0101@gmail.com>
2024-09-07 10:07:28 -0600

    Primer Commit de intro-ts

---
+++ src/temas-ts/01-tipos-bas.ts
@@ -0,0 +1,14 @@
+var num1=23;
+let num2=12;
+const num3=34;
+
+const sum= num1+num2+num3;
+console.log(`La suma es: ${sum}`);
+
+let nombre:string= 'Juan';
+let edad:number= 21;
+let activo:boolean= true;
+
+let matricula:number|string;//admite numeros y letras
+matricula=123456;
+matricula='63gsgt889'
